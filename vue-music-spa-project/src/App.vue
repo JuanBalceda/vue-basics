257fd6f2609d971c87d16fb38becfd642e0f5fdf
Juan Balceda <juanbalceda@gmail.com>
2019-04-07 22:23:37 +0200

    Added Country selector

--- vue-music-spa-project/src/App.vue
+++ vue-music-spa-project/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>{{ name }}</h1>
+    <select v-model="selectedCountry">
+      <option v-for="country in countries" v-bind:value="country.name" :key="country.alpha2Code">{{ country.name }}</option>
+    </select>
+    <spinner v-show="loading"></spinner>
     <ul>
       <artist v-for="artist in artists" :artist="artist" :key="artist.mbid"></artist>
     </ul>
@@ -8,27 +12,49 @@
 </template>
 
 <script>
-import Artist from './components/Artist.vue'
-import getArtists from "./api";
+import Artist from "./components/Artist.vue"
+import Spinner from "./components/Spinner.vue"
+import { getArtists, getCountries } from "./api"
 
 export default {
   name: "app",
   data() {
     return {
       name: "Vue Music",
-      artists: []
-    };
+      artists: [],
+      countries: [],
+      selectedCountry: "Peru",
+      loading: true
+    }
   },
   components: {
-    Artist
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists() {
+      const self = this
+      this.loading = true
+      this.artists = []
+      getArtists(this.selectedCountry).then(function(artists) {
+        self.loading = false
+        self.artists = artists
+      })
+    }
   },
   mounted: function() {
-    const self = this;
-    getArtists().then(function(artists) {
-      self.artists = artists;
-    });
+    const self = this
+    this.refreshArtists()
+    getCountries().then(function(countries) {
+      self.countries = countries
+    })
+  },
+  watch: {
+    selectedCountry: function() {
+      this.refreshArtists()
+    }
   }
-};
+}
 </script>
 
 <style>
--- vue-music-spa-project/src/api/index.js
+++ vue-music-spa-project/src/api/index.js
@@ -1,10 +1,26 @@
 import config from './config'
 
-const { apiKey } = config
-const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=peru&api_key=${apiKey}&format=json`
+const {
+  apiKey
+} = config
 
-export default function getArtists(){
-    return fetch(url)
+const url_artists = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json`
+const url_countries = `https://restcountries.eu/rest/v2/all`
+
+function getCountries() {
+  return fetch(url_countries)
+    .then(res => res.json())
+    .then(json => json)
+}
+
+function getArtists(country) {
+  const url = url_artists.replace(':country', country)
+  return fetch(url)
     .then(res => res.json())
     .then(json => json.topartists.artist)
+}
+
+export {
+  getCountries,
+  getArtists
 }
